fe9ee60fd5e71ba4b90c393d8cf57bcd1e1191c0
ajay-nooji <ajaynooji@gmail.com>
2024-06-07 20:52:18 +0530

    Files Deleted

--- flet-app-3/README.md
+++
@@ -1,9 +0,0 @@
-# A flet-app-3 Flet app
-
-An example of a minimal Flet app. The text is in green color.
-
-To run the app:
-
-```
-flet run [app_directory]
-```
--- flet-app-3/main.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    t=ft.Text(value="Hello World!", color="green")
-    page.controls.append(t)
-    page.update()
-    
-ft.app(target=main)
